2080538d470e20ebd030f84b892b2a4c1218f5ff
jonsth131 <jonas1111@gmail.com>
2023-10-23 18:23:18 +0200

    Added deadface ctf 2023

---
+++ deadface/2023/cereal-killer-01/solve.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+flag = "I&_9a%mx_tRmE4D3DmYw_9fbo6rd_aFcRbE,D.D>Y[!]!'!q"
+
+print('flag{', end="")
+for i in range(0, len(flag), 2):
+    print(flag[i], end="")
+
+print('}')
